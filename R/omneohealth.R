b2b4dead3ceef41fd755680cf789cd35230b66cf
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2020-01-15 12:40:06 +0000

    add package description script

---
+++ R/omneohealth.R
@@ -0,0 +1,18 @@
+################################################################################
+#
+#' The Oxford Maternal and Neonatal Health study aims to synthesise the current
+#' evidence on maternal and neonatal health in various countries through
+#' systematic collection of related secondary data from multiple sources. Then
+#' based on this synthesis, gaps in knowledge and corresponding data are
+#' identified in order to inform researchers on possible future areas of study.
+#' This package brings together all the analytic functions created and used for
+#' this study.
+#'
+#' @docType package
+#' @keywords internal
+#' @name omneohealth
+#'
+#'
+#
+################################################################################
+"_PACKAGE"
